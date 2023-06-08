d43dfb8e6325cedb398942398de5c267a0d203db
samSargsyan <samuelsargsyan2010@gmail.com>
2023-06-07 20:56:30 -0700

    ss-games

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Routes>
-          <Route path="/" element={<Content />} />
+          <Route path="/" index element={<Content />} />
           <Route path="/:game" element={<GameInfo />} />
       </Routes>
     </div>
