0237c3a2770d21325d68b747494c2e68e449384d
samSargsyan <samuelsargsyan2010@gmail.com>
2023-06-14 20:51:01 -0700

    search no result

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -3,19 +3,27 @@ import "./content.scss";
 import data from "../../data";
 
 const Content = ({search}) => {
-console.log(search);
+
+    const filtered = data.filter((item)=>{
+            return item.title.toLowerCase().includes(search.toLowerCase());
+        })
+    
     return(
         <div className="content">
             {
-                data
-                .filter((item)=>{
-                    return item.title.toLowerCase().includes(search.toLowerCase());
-                })
-                .map((item)=>{
-                    return <Game gif={item.gif} image={item.image} title={item.title} category={item.category} />
-                })
+                filtered.length === 0 ?
+                    <div>
+                        <span>No Games Found Under the Name of "{search}", If you have any Games you would like to see on the website, 
+                        Leave a Suggestion to the following email <a href="mailto:samuelsargsyan2010@gmail.com">Send Email</a>.</span>
+                    </div>
+                :
+                    filtered
+                    .map((item)=>{
+                        return <Game gif={item.gif} image={item.image} title={item.title} category={item.category} />
+                    })
             }
         </div>
+
     )
 }
 
